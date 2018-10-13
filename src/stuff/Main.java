f0b49f1d00df6512a61e09e4097b3ec33228a9de
Fredrik Sjögren <Fredrik@Fredriks-MacBook-Pro.local>
2018-10-13 15:29:02 +0200

    Added the office :).

--- src/stuff/Main.java
+++ src/stuff/Main.java
@@ -6,9 +6,23 @@ public class Main {
 	    int a = 5;
 	    int b = 10;
 	    someShittyMethod(a, b);
+
     }
 
     public static void someShittyMethod(int a, int b){
         System.out.println(a*b+b);
     }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+

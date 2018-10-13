dd14b21a7add114d97db9ba7d7ad2e401c7a3757
august axelsson <augustaxelsson1@augusts-MacBook-Pro-2.local>
2018-10-13 15:17:52 +0200

    added

--- src/stuff/Main.java
+++ src/stuff/Main.java
@@ -2,13 +2,20 @@ package stuff;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 	    int a = 5;
 	    int b = 10;
 	    someShittyMethod(a, b);
     }
 
-    public static void someShittyMethod(int a, int b){
+    public static void someShittyMethod(int a, int b) {
         System.out.println(a*b+b);
     }
+
+
+
+
+
 }

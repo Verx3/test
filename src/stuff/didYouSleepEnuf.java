52be65433a7be81ab3b70d9fb45d31f1c909fc5c
Clem <Clemens.lans@live.com>
2018-10-13 17:26:33 +0200

    uhh I'm definintely not just trying to get one step closer to a shirt

--- src/stuff/didYouSleepEnuf.java
+++ src/stuff/didYouSleepEnuf.java
@@ -2,11 +2,13 @@ package test.src.stuff;
 
 public class didYouSleepEnuf {
 
-    public void isItEnuf() {
-        if(number < 7){
-            System.out.println("uh oh, you should sleep more");
+    public void isItEnuf(double number) {
+        if(number < 3){
+            System.out.println("holy shit dude you need jesus");
+        } else if(number < 7){
+            System.out.println("wtf man just hit the snooze more often");
         } else {
-            System.out.println("I think you need at least 2 more hours, just hit the snooze");
+            System.out.println("AT LEAST 2 more hours before it's worth getting out of bed");
         }
     }
 }
